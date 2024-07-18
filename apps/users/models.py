5393c616364daafa8c17ebf0322a192e24520889
Muhammad Bilal <bilalarshad@nexquery.com>
2024-07-19 01:23:13 +0500

    Project setup

---
+++ apps/users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+from apps.users.managers import MyUserManager
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+        null=False,
+        blank=False
+    )
+    username = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email

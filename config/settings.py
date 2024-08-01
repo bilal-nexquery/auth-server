883c00fbedb0a69e23793374f0518c99d473cd68
Muhammad Bilal <bilalarshad@nexquery.com>
2024-08-01 14:29:46 +0500

    Trying to deploy on railway

--- config/settings.py
+++ config/settings.py
@@ -209,6 +209,14 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
+
+MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

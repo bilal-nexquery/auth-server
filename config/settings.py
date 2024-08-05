572b27bd66be1d25bb82ffe20e387036eef59d5c
Muhammad Bilal <bilalarshad@nexquery.com>
2024-08-05 17:49:36 +0500

    testing cookie conf

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,14 @@ AUTH_USER_MODEL = "users.User"
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
+# Cors configuration
+CORS_ALLOWED_ORIGINS = ["https://auth-client-psi.vercel.app", "http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
+
+# Cookie settings
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -206,14 +214,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-# Cors configuration
-CORS_ALLOWED_ORIGINS = ["https://auth-client-psi.vercel.app", "http://localhost:3000"]
-CORS_ALLOW_CREDENTIALS = True
-
-# Cookie settings
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
